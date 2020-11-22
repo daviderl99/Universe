36ff681c57101e32e7beb10dc4c187d9ef4a2600
daviderl99 <daviderlich7@hot.ee>
2020-11-22 19:33:52 +0200

    Added planets, mesa radii, selection marker

--- Star.js
+++ Star.js
@@ -3,9 +3,11 @@ class Star{
   constructor(x, y){
     this.x = x;
     this.y = y;
-    this.r = genRadius();
+    this.r = genRadius(1, 10);
+    this.mesaR = genMesaRadius(this.r);
     this.color = genColor();
-    this.name = genName();
+    this.name = genName(4, 10);
+    this.planets = genPlanets(0, 10);
   }
 
   show(){
@@ -13,60 +15,4 @@ class Star{
     fill(this.color);
     ellipse(this.x, this.y, this.r);
   }
-}
-
-function genRadius(){
-  return randomInt(1, 10);
-}
-
-function genColor(){
-  hue = (Math.random() > 0.5) ? randomInt(0, 55) : randomInt(200, 250);
-  sat = (Math.random() > 0.3) ? 75 : 1;
-  bri = 100;
-  return color(hue, sat, bri);
-}
-
-function genName(){
-  const vowels = "aeiou".split("");
-  const consonants = "bcdfghjklmnpqrstvwxyz".split("");
-  nameLength = randomInt(4, 10);
-  let name = "";
-
-  while (name.length < nameLength){
-    if (Math.random() > 0.5) {
-      if (lastLetters(name, vowels, 2)) {
-        name = addLetter(name, consonants);
-        continue;
-      }
-      name = addLetter(name, vowels);
-    } else {
-      if (lastLetters(name, consonants, 2)) continue;
-      name = addLetter(name, consonants);
-    }
-  }
-  return capitalizeFirstLetter(name);
-}
-
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function addLetter(name, type){
-  index = randomInt(0, type.length-1);
-  name += type[index];
-  return name;
-}
-
-function lastLetters(word, letters, count){
-  chars = word.slice(-count).split("");
-  for (c of chars){
-    if (!letters.includes(c)){
-      return false;
-    }
-  }
-  return true;
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
 }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let density = 0.0002;
 let stars = [];
 
+sunSelection = {"x": null, "y": null, "r": null};
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 100);
   createStars();
 }
   
@@ -11,6 +12,11 @@ function draw(){
   background(0);  
   drawStars();
   overStar();
+  
+  const exists = Object.values(sunSelection).every(x => (x !== null));
+  if (exists){
+    drawSelection(sunSelection.x, sunSelection.y, sunSelection.r);
+  }
 }
 
 function drawStars(){
@@ -38,6 +44,7 @@ function overStar(){
       fill(255);
       textSize(24);
       text(star.name, mouseX, mouseY);
+      break;
     }
   }
 }
@@ -48,7 +55,10 @@ function mouseClicked(){
     distance = dist(mouseX, mouseY, star.x, star.y);
     if (distance < star.r){
       info.style.display = "block";
-      console.log(info.style.display);
+      insertData(star);
+      sunSelection.x = star.x;
+      sunSelection.y = star.y;
+      sunSelection.r = star.r;
     }
   }
 }
@@ -56,4 +66,41 @@ function mouseClicked(){
 function closeInfoBox(){
   info = document.getElementById("info-box");
   info.style.display = "none";
+}
+
+function insertData(star){
+  document.getElementById("star-name").innerHTML = "Name: " + star.name;
+  document.getElementById("star-radius").innerHTML = "Radius: " + star.mesaR + " km";
+  document.getElementById("planet-count").innerHTML = "Planets: " + star.planets.length;
+}
+
+function drawSelection(x, y, r){
+  smallVal = 1.4;
+  bigVal = 5;
+  noStroke();
+  fill(255, 0, 0);
+  beginShape();
+    vertex(x - r, y - r);
+    vertex(x - r, y - r/bigVal);
+    vertex(x - r*smallVal, y - r*smallVal);
+    vertex(x - r/bigVal, y - r);
+  endShape();
+  beginShape();
+    vertex(x + r, y - r);
+    vertex(x + r, y - r/bigVal);
+    vertex(x + r*smallVal, y - r*smallVal);
+    vertex(x + r/bigVal, y - r);
+  endShape();
+  beginShape();
+    vertex(x + r, y + r);
+    vertex(x + r, y + r/bigVal);
+    vertex(x + r*smallVal, y + r*smallVal);
+    vertex(x + r/bigVal, y + r);
+  endShape();
+  beginShape();
+    vertex(x - r, y + r);
+    vertex(x - r, y + r/bigVal);
+    vertex(x - r*smallVal, y + r*smallVal);
+    vertex(x - r/bigVal, y + r);
+  endShape();
 }
